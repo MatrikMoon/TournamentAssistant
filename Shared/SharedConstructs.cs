177dd74c8f4510a4ffc537ba89be7a88cc87ac00
Moon <q@q>
2020-04-30 15:11:55 -0500

    Fixed song downloading issue

--- Shared/SharedConstructs.cs
+++ Shared/SharedConstructs.cs
@@ -8,12 +8,13 @@ namespace BattleSaberShared
     public static class SharedConstructs
     {
         public const string Name = "BattleSaber";
-        public const string Version = "0.1.1";
-        public const int VersionCode = 011;
+        public const string Version = "0.1.2";
+        public const int VersionCode = 012;
         public static string Changelog =
             "0.0.1: Begin assembling UI for coordinator panels\n" +
             //Whoops
-            "0.1.1: Implemented versioning system\n";
+            "0.1.1: Implemented versioning system\n" + 
+            "0.1.2: Fixed song download bug\n";
 
         public enum BeatmapDifficulty
         {
