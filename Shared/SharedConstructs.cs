84975858c920a4cbceb3881e2735e64a3a2cf0a8
Moon <q@q>
2020-05-02 22:43:46 -0500

    Version bump

--- Shared/SharedConstructs.cs
+++ Shared/SharedConstructs.cs
@@ -8,13 +8,14 @@ namespace BattleSaberShared
     public static class SharedConstructs
     {
         public const string Name = "BattleSaber";
-        public const string Version = "0.1.2";
-        public const int VersionCode = 012;
+        public const string Version = "0.1.3";
+        public const int VersionCode = 013;
         public static string Changelog =
             "0.0.1: Begin assembling UI for coordinator panels\n" +
             //Whoops
             "0.1.1: Implemented versioning system\n" + 
-            "0.1.2: Fixed song download bug\n";
+            "0.1.2: Fixed song download bug\n" +
+            "0.1.3: Recreated song detail view, refactored tournament flowcoordinator into room flowcoordinator, added match destroying / player leaving to back button on BattleSaber side\n";
 
         public enum BeatmapDifficulty
         {
