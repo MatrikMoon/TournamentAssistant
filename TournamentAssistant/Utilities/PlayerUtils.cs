5dd20aaa73df271c8d4e1e3666c54136984873be
Moon <q@q>
2020-11-09 21:27:58 -0600

    Fixed Oculus bug

--- TournamentAssistant/Utilities/PlayerUtils.cs
+++ TournamentAssistant/Utilities/PlayerUtils.cs
@@ -1,44 +1,17 @@
-﻿using TournamentAssistant.Misc;
-using Oculus.Platform;
-using Oculus.Platform.Models;
-using Steamworks;
+﻿using BS_Utils.Gameplay;
 using System;
 using System.Linq;
+using TournamentAssistant.Misc;
 using UnityEngine;
 
 namespace TournamentAssistant.Utilities
 {
     public class PlayerUtils
     {
-        public static void GetPlatformUserData(Action<string, ulong> usernameResolved)
+        public static async void GetPlatformUserData(Action<string, ulong> usernameResolved)
         {
-            if (SteamManager.Initialized)
-            {
-                GetSteamUser(usernameResolved);
-            }
-            else
-            {
-                GetOculusUser(usernameResolved);
-            }
-        }
-
-        private static void GetSteamUser(Action<string, ulong> usernameResolved)
-        {
-            if (SteamManager.Initialized)
-            {
-                usernameResolved?.Invoke(SteamFriends.GetPersonaName(), SteamUser.GetSteamID().m_SteamID);
-            }
-        }
-
-        private static void GetOculusUser(Action<string, ulong> usernameResolved)
-        {
-            Users.GetLoggedInUser().OnComplete((Message<User> msg) =>
-            {
-                if (!msg.IsError)
-                {
-                    usernameResolved?.Invoke(msg.Data.OculusID, msg.Data.ID);
-                }
-            });
+            var user = await GetUserInfo.GetUserAsync();
+            usernameResolved?.Invoke(user.userName, ulong.Parse(user.platformUserId));
         }
 
         public static void ReturnToMenu()
