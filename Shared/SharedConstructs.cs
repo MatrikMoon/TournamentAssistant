e5c6357a51d84ec0f91d68eb22e9111a770d349f
MatrikMoon <5924350+MatrikMoon@users.noreply.github.com>
2021-02-05 00:01:34 -0600

    Bump version

--- Shared/SharedConstructs.cs
+++ Shared/SharedConstructs.cs
@@ -8,8 +8,8 @@ namespace TournamentAssistantShared
     public static class SharedConstructs
     {
         public const string Name = "TournamentAssistant";
-        public const string Version = "0.4.0";
-        public const int VersionCode = 040;
+        public const string Version = "0.4.1";
+        public const int VersionCode = 041;
         public static string Changelog =
             "0.0.1: Begin assembling UI for coordinator panels\n" +
             "0.1.1: Implemented versioning system\n" +
@@ -35,7 +35,8 @@ namespace TournamentAssistantShared
             "0.3.6: Changed to hub and spoke style network, bump TAUI version, fix accuracy for overlay\n" +
             "0.3.7: Added password support and disabled score submission when nofail is on\n" +
             "0.3.8: Fixed qualifier flow coordinator lock-in, partially fixed custom leaderboards\n" +
-            "0.4.0: Version bump to 1.13.2, merged websocket server, fixed quals leaderboard, re-mesh-networked for event scraping purposes";
+            "0.4.0: Version bump to 1.13.2, merged websocket server, fixed quals leaderboard, re-mesh-networked for event scraping purposes\n" +
+            "0.4.1: Fixed a few quals ui bugs, merged player settings page";
 
         public enum BeatmapDifficulty
         {
