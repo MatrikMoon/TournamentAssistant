a5c70405869fe93393fe29cfe5f6f676db68978e
MatrikMoon <5924350+MatrikMoon@users.noreply.github.com>
2021-04-20 01:08:34 -0500

    Fix logging prefix

--- Shared/Logger.cs
+++ Shared/Logger.cs
@@ -12,6 +12,12 @@ namespace TournamentAssistantShared
 {
     class Logger
     {
+#if DEBUG
+        public static bool DEBUG = true;
+#else
+        public static bool DEBUG = false;
+#endif
+
         //Added for the purpose of viewing log info in the UI
         public enum LogType
         {
@@ -24,60 +30,57 @@ namespace TournamentAssistantShared
 
         public static event Action<LogType, string> MessageLogged;
 
-        public static void Error(object message)
+        private static string GetPrefix()
         {
-            MessageLogged?.Invoke(LogType.Error, message.ToString());
+            return $"[{SharedConstructs.Name} {DateTime.UtcNow}]: ";
+        }
+
+        public static void Error(string message)
+        {
+            MessageLogged?.Invoke(LogType.Error, message);
             ConsoleColor originalColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(message);
+            Console.WriteLine(GetPrefix() + message);
             Console.ForegroundColor = originalColor;
         }
 
-        public static void Warning(object message)
+        public static void Warning(string message)
         {
-            MessageLogged?.Invoke(LogType.Warning, message.ToString());
+            MessageLogged?.Invoke(LogType.Warning, message);
             ConsoleColor originalColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(message);
+            Console.WriteLine(GetPrefix() + message);
             Console.ForegroundColor = originalColor;
         }
 
-        public static void Info(object message)
+        public static void Info(string message)
         {
-            MessageLogged?.Invoke(LogType.Info, message.ToString());
+            MessageLogged?.Invoke(LogType.Info, message);
             ConsoleColor originalColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine(message);
+            Console.WriteLine(GetPrefix() + message);
             Console.ForegroundColor = originalColor;
         }
 
-        public static void Success(object message)
+        public static void Success(string message)
         {
-            MessageLogged?.Invoke(LogType.Success, message.ToString());
+            MessageLogged?.Invoke(LogType.Success, message);
             ConsoleColor originalColor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(message);
+            Console.WriteLine(GetPrefix() + message);
             Console.ForegroundColor = originalColor;
         }
 
-        public static void Debug(object message)
+        public static void Debug(string message)
         {
-#if DEBUG
-            MessageLogged?.Invoke(LogType.Debug, message.ToString());
-            ConsoleColor originalColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine(message);
-            Console.ForegroundColor = originalColor;
-#endif
-        }
-
-        public static void ColoredLog(object message, ConsoleColor color)
-        {
-            MessageLogged?.Invoke(LogType.Info, message.ToString());
-            ConsoleColor originalColor = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            Console.WriteLine(message);
-            Console.ForegroundColor = originalColor;
+            if (DEBUG)
+            {
+                MessageLogged?.Invoke(LogType.Debug, message);
+                ConsoleColor originalColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(GetPrefix() + message);
+                Console.ForegroundColor = originalColor;
+            }
         }
     }
 }
