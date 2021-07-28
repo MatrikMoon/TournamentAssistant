410703910dc192f0883081db547fe07035c71d06
Auros Nexus <aurosnex@gmail.com>
2021-07-28 14:10:50 -0400

    Screen Overlay

--- TournamentAssistant/Behaviors/ScreenOverlay.cs
+++ TournamentAssistant/Behaviors/ScreenOverlay.cs
@@ -1,53 +1,55 @@
-﻿using BeatSaberMarkupLanguage;
-using System.Linq;
-using TournamentAssistant.Misc;
+﻿using IPA.Utilities.Async;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace TournamentAssistant.Behaviors
 {
-    class ScreenOverlay : MonoBehaviour
+    internal class ScreenOverlay : MonoBehaviour
     {
-        public static ScreenOverlay Instance { get; set; }
+        private Canvas _overlayCanvas = null!;
+        private RawImage _overlayImage = null!;
+        private byte[] imageBytes = Array.Empty<byte>();
 
-        private Canvas _overlayCanvas;
-        private RawImage _overlayImage;
-        private byte[] imageBytes;
-
-        void Awake()
+        protected void Awake()
         {
-            Instance = this;
-            DontDestroyOnLoad(this);
-        }
+            _overlayCanvas = gameObject.AddComponent<Canvas>();
+            _overlayCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            _overlayImage = gameObject.AddComponent<RawImage>();
+            _overlayCanvas.sortingOrder = 5001;
 
-        public void Clear()
-        {
-            if (_overlayImage != null) _overlayImage.color = Color.clear;
+            _overlayImage.rectTransform.SetParent(_overlayCanvas.transform, false);
+            _overlayImage.material = BeatSaberMarkupLanguage.Utilities.ImageResources.NoGlowMat;
+            _overlayImage.color = Color.clear;
+            _overlayCanvas.enabled = false;
+            _overlayImage.enabled = false;
         }
 
         public void ShowPng()
         {
-            UnityMainThreadDispatcher.Instance().Enqueue(() =>
+            UnityMainThreadTaskScheduler.Factory.StartNew(() =>
             {
-                _overlayCanvas ??= gameObject.AddComponent(Resources.FindObjectsOfTypeAll<Canvas>().First(x => x.name == "DropdownTableView"));
-                _overlayCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
-
-                _overlayCanvas.overrideSorting = true;
-                _overlayCanvas.sortingOrder = Resources.FindObjectsOfTypeAll<Canvas>().Length + 1;
-
-                _overlayImage ??= _overlayCanvas.gameObject.AddComponent<RawImage>();
-                var imageTransform = _overlayImage.transform as RectTransform;
-                imageTransform.SetParent(_overlayCanvas.transform, false);
-                _overlayImage.material = Resources.FindObjectsOfTypeAll<Material>().FirstOrDefault(x => x.name == "UINoGlow");
-
                 if (imageBytes != null)
                 {
+                    _overlayImage.enabled = true;
+                    _overlayCanvas.enabled = true;
                     var texture = new Texture2D(1, 1);
                     ImageConversion.LoadImage(texture, imageBytes);
                     _overlayImage.color = Color.white;
                     _overlayImage.texture = texture;
                 }
             });
+        }
+
+        public void Clear()
+        {
+            if (_overlayImage != null)
+            {
+                _overlayImage.color = Color.clear;
+                _overlayCanvas.enabled = false;
+                _overlayImage.enabled = false;
+            }
+
         }
 
         public void SetPngBytes(byte[] pngBytes)
---
+++ TournamentAssistant/Models/RoomData.cs
@@ -0,0 +1,16 @@
+﻿using TournamentAssistantShared.Models;
+
+namespace TournamentAssistant.Models
+{
+    public class RoomData
+    {
+        public readonly Match match;
+        public readonly bool tournamentMode;
+
+        public RoomData(Match match, bool tournamentMode)
+        {
+            this.match = match;
+            this.tournamentMode = tournamentMode;
+        }
+    }
+}
