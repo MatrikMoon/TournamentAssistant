ccb7889f01d818fd096c5eb5acb710170ae709c0
Arimodu <Samuel.luxik03@gmail.com>
2021-08-11 16:31:30 +0200

    Created static GlobalConstants class
    This class houses references to APIs and system path references
    Usage:
    Include "using static TournamentAssistantShared.GlobalConstants" in any class to reference

---
+++ TournamentAssistantShared/GlobalConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TournamentAssistantShared
+{
+    public static class GlobalConstants
+    {
+        public const string BeatsaverCDN = "https://cdn.beatsaver.com/";
+        public const string BeatsaverAPI = "https://api.beatsaver.com/";
+        public const string MapInfoByID = "https://api.beatsaver.com/maps/id/";
+        public const string MapInfoByHash = "https://api.beatsaver.com/maps/hash/";
+        public static string SystemPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\TournamentAssistantUI";
+        public static string Temp = $"{SystemPath}\\temp";
+        public static string cache = $"{SystemPath}\\cache";
+        public static string SongData = $"{SystemPath}\\SongData";
+        public static char[] IllegalCharacters = { '>', '<', ':', '/', '\\', '\"', '|', '?', '*', ' ' };
+    }
+}
