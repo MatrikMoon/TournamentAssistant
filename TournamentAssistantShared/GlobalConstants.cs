97b6a2f9ce91197f72c62408ee2679bd1008d690
Arimodu <Samuel.luxik03@gmail.com>
2021-08-11 16:43:04 +0200

    Update GlobalConstants.cs

--- TournamentAssistantShared/GlobalConstants.cs
+++ TournamentAssistantShared/GlobalConstants.cs
@@ -1,19 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TournamentAssistantShared
 {
     public static class GlobalConstants
     {
-        public const string BeatsaverCDN = "https://cdn.beatsaver.com/";
-        public const string BeatsaverAPI = "https://api.beatsaver.com/";
-        public const string MapInfoByID = "https://api.beatsaver.com/maps/id/";
-        public const string MapInfoByHash = "https://api.beatsaver.com/maps/hash/";
-        public static string SystemPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\TournamentAssistantUI";
-        public static string Temp = $"{SystemPath}\\temp";
-        public static string cache = $"{SystemPath}\\cache";
-        public static string SongData = $"{SystemPath}\\SongData";
-        public static char[] IllegalCharacters = { '>', '<', ':', '/', '\\', '\"', '|', '?', '*', ' ' };
+        public static string BeatsaverCDN => "https://cdn.beatsaver.com/"; 
+        public static string BeatsaverAPI => "https://api.beatsaver.com/";
+        public static string MapInfoByID => "https://api.beatsaver.com/maps/id/";
+        public static string MapInfoByHash => "https://api.beatsaver.com/maps/hash/";
+        public static string SystemPath => $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\TournamentAssistant";
+        public static string Temp => $"{SystemPath}\\temp";
+        public static string Cache => $"{SystemPath}\\cache";
+        public static string SongData => $"{SystemPath}\\SongData";
+        public static char[] IllegalCharacters => _illegalCharacters;
+
+
+        
+        private static char[] _illegalCharacters = { '>', '<', ':', '/', '\\', '\"', '|', '?', '*', ' ' };
     }
 }
