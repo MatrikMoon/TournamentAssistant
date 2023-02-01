72ee5505f546f9dc36c40c26b5c7aced4d33d261
Moon <5924350+MatrikMoon@users.noreply.github.com>
2023-01-31 21:48:22 -0600

    Cleanup

--- TournamentAssistantUI/src/routes/+page.svelte
+++ TournamentAssistantUI/src/routes/+page.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import TournamentSelection from "../lib/pages/TournamentSelection.svelte";
-
-  function onTournamentSelected(id: string) {
-    goto(`/tournament?id=${id}`);
-  }
 </script>
 
 <div>
-  <TournamentSelection {onTournamentSelected} />
+  <TournamentSelection
+    onTournamentSelected={(id) => goto(`/tournament?id=${id}`)}
+  />
 </div>
