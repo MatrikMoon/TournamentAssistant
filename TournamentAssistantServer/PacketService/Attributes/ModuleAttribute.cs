e61fb55786a85fdc35b0ad81303399371fae0c8e
Moon <5924350+MatrikMoon@users.noreply.github.com>
2024-04-19 11:21:26 -0500

    Fix team image saving, publish step

--- TournamentAssistantServer/PacketService/Attributes/ModuleAttribute.cs
+++ TournamentAssistantServer/PacketService/Attributes/ModuleAttribute.cs
@@ -5,27 +5,33 @@ using TournamentAssistantShared.Models.Packets;
 namespace TournamentAssistantServer.PacketService.Attributes
 {
     [AttributeUsage(AttributeTargets.Class, Inherited = false)]
-    public class ModuleAttribute(Packet.packetOneofCase packetType, string switchTypePath = "") : Attribute
+    public class ModuleAttribute : Attribute
     {
-        public Packet.packetOneofCase PacketType { get; private set; } = packetType;
+        public Packet.packetOneofCase PacketType { get; private set; }
 
         // Each packet has a differently named and typed Enum which
         // defines exactly which handler should be run. Since it's
         // differently named and typed, we'll compare it to an int
         // (thank god they're enums), and force the attribute provider
         // to give us a stub which returns the value we should switch by
-        public Func<Packet, int> GetSwitchType { get; private set; } = (packet) =>
+        public Func<Packet, int> GetSwitchType { get; private set; }
+
+        public ModuleAttribute(Packet.packetOneofCase packetType, string switchTypePath = "")
         {
-            if (string.IsNullOrEmpty(switchTypePath))
+            PacketType = packetType;
+            GetSwitchType = (packet) =>
             {
-                return 0;
-            }
+                if (string.IsNullOrEmpty(switchTypePath))
+                {
+                    return 0;
+                }
 
-            var pathWithoutPacket = switchTypePath.StartsWith("packet.") ? switchTypePath.Substring("packet.".Length) : switchTypePath;
-            var pathParts = pathWithoutPacket.Split(".");
+                var pathWithoutPacket = switchTypePath.StartsWith("packet.") ? switchTypePath.Substring("packet.".Length) : switchTypePath;
+                var pathParts = pathWithoutPacket.Split(".");
 
-            var switchType = packet.GetProperty(pathParts[0]).GetProperty<int>(pathParts[1]);
-            return switchType;
-        };
+                var switchType = packet.GetProperty(pathParts[0]).GetProperty<int>(pathParts[1]);
+                return switchType;
+            };
+        }
     }
 }
--- TournamentAssistantServer/PacketService/Attributes/PacketHandlerAttribute.cs
+++ TournamentAssistantServer/PacketService/Attributes/PacketHandlerAttribute.cs
@@ -3,8 +3,13 @@
 namespace TournamentAssistantServer.PacketService.Attributes
 {
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
-    public class PacketHandlerAttribute(int switchType = 0) : Attribute
+    public class PacketHandlerAttribute : Attribute
     {
-        public int SwitchType { get; private set; } = switchType;
+        public int SwitchType { get; private set; }
+
+        public PacketHandlerAttribute(int switchType = 0)
+        {
+            SwitchType = switchType;
+        }
     }
 }
