05703579f956e00d48a67c61ff85a7d0eb0547d0
Luna <palas.balazs@gmail.com>
2026-09-15 06:01:21 +0200

    fix: bump standalone version to 1.3.2???

--- TournamentAssistantStandalone/include/TA/Constants.hpp
+++ TournamentAssistantStandalone/include/TA/Constants.hpp
@@ -7,7 +7,7 @@ namespace TA {
     constexpr const char* kServerHost = "server.tournamentassistant.net";
     constexpr int kServerPort = 8675;
     constexpr int kMasterApiPort = 8678;
-    constexpr int kClientVersion = 1300;
+    constexpr int kClientVersion = 1320;
     constexpr const char* kBeatKhanaGameAuthUrl = "https://api.beatkhana.com/game/requestAuthToken";
     constexpr const char* kLatestStandaloneVersionUrl = "https://api.beatkhana.com/game/ta/standalone/latest-version";
     constexpr const char* kDownloadUrl = "https://download.tournamentassistant.net";
